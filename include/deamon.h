f98e8247cd4c3fb559d046072fa4dd131362e336
mathieu <mathieu.hautebas@gmail.com>
2018-05-09 22:37:17 +0200

    too lazy to git diff but I'm sure there are changes

--- include/deamon.h
+++ include/deamon.h
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 bool file_exists(const char* file);
 int deamonize(const char *pidfile);
 int get_instance_pid(const char *file);
--- src/main.c
+++ src/main.c
@@ -10,18 +10,26 @@
 void usage(const char *toolname){
 	fprintf(stdout, "usage : %s [OPTIONS]\n", toolname);
 	fprintf(stdout, " -s\tstop deamon\n");
+	fprintf(stdout, " -n\tno daemon\n");
+	fprintf(stdout, " -r\treset\n");
 	fprintf(stdout, " -h\thelp\n");
 }
 
 int main(int argc, char const *argv[])
-{	
+{
 	const char *pidfile = "dcsd_status.pid";
-	int stop = 0, dcsd_status_pid = 0;
+	int stop = 0, no_deamon = 0;
+	int dcsd_status_pid = 0;
 
 	for(int i = 0; i < argc; i++) {
 		if (!strcmp(argv[i], "-s"))
 		{
 			stop = 1;
+		} else if (!strcmp(argv[i], "-r")){
+			set_led(4);
+			return 0;
+		} else if (!strcmp(argv[i], "-n")){
+			no_deamon = 1;
 		} else if (!strcmp(argv[i], "-h")){
 			usage(argv[0]);
 			return 0;
@@ -30,22 +38,27 @@ int main(int argc, char const *argv[])
 
 	if (stop){
 		dcsd_status_pid = get_instance_pid(pidfile);
-		printf("%d\n", dcsd_status_pid);
 
 		/* No need to kill something that does not exist*/
 		if (dcsd_status_pid == -1)
 			return 0;
 
+		printf("%d\n", dcsd_status_pid);
 		remove(pidfile);
 		kill(dcsd_status_pid, SIGINT);
 		printf("[i] stopped\n");
 		return 0;
 	}
 
-	deamonize(pidfile);
-	while(1){
+	if (no_deamon) {
 		set_led(device_mode());
-		sleep(1);
+	}
+	else {
+		deamonize(pidfile);
+		while(1){
+			set_led(device_mode());
+			sleep(1);
+		}
 	}
 	return 0;
 }
