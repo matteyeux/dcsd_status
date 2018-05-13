6760a7605891cfc467e7c85b048abb05e8fe2e09
matteyeux <mathieu.hautebas@gmail.com>
2018-05-13 03:00:54 -0700

    use fprintf and add 'symbols'

--- src/dcsd_status.c
+++ src/dcsd_status.c
@@ -21,28 +21,28 @@ int device_mode(void) {
 	devicemode = NORM_MODE;
 	device = libusb_open_device_with_vid_pid(NULL, VENDOR_ID, devicemode);
 	if (device != NULL){
-		fprintf(stdout, "normal\n");
+		fprintf(stdout, "[i] state : normal\n");
 		return 1;
 	}
 
 	devicemode = RECV_MODE;
 	device = libusb_open_device_with_vid_pid(NULL, VENDOR_ID, devicemode);
 	if (device != NULL){
-		fprintf(stdout, "recovery\n");
+		fprintf(stdout, "[i] state : recovery\n");
 		return 2;
 	}
 
 	devicemode = WTF_MODE;
 	device = libusb_open_device_with_vid_pid(NULL, VENDOR_ID, devicemode);
 	if (device != NULL){
-		fprintf(stdout, "dfu\n");
+		fprintf(stdout, "[i] state : dfu\n");
 		return 3;
 	}
 
 	devicemode = DFU_MODE;
 	device = libusb_open_device_with_vid_pid(NULL, VENDOR_ID, devicemode);
 	if (device != NULL){
-		fprintf(stdout, "dfu\n");
+		fprintf(stdout, "[i] state : dfu\n");
 		return 3;
 	}
 	return 0;
@@ -60,11 +60,11 @@ int set_led(int led){
 	int f;
 	long int tab[5] = {0xF0, 0xF2, 0xF8, 0xF1, 0xFB};
 	unsigned char buf[1];
-	static int led_status = 0;
+	static int led_status = -1;
 
 	if (led == led_status)
 	{
-		fprintf(stdout, "state is unchanged, not doing anything\n");
+		fprintf(stdout, "[i] state is unchanged, not doing anything\n");
 		return 0;
 	} else {
 		led_status = led;
@@ -74,7 +74,7 @@ int set_led(int led){
 	ftdi = ftdi_new();
 
 	if (ftdi == 0){
-		fprintf(stderr, "ftdi_new failed\n");
+		fprintf(stderr, "[e] ftdi_new failed\n");
 		return -1;
 	}
 	/* 0x0403 : vendor ID */
@@ -83,17 +83,17 @@ int set_led(int led){
 
 	while (f < 0){
 		f = ftdi_usb_open(ftdi, 0x0403, 0x8a88);
-		fprintf(stderr, "unable to open ftdi device: %d (%s)\n", f, ftdi_get_error_string(ftdi));
+		fprintf(stderr, "[e] unable to open ftdi device: %d (%s)\n", f, ftdi_get_error_string(ftdi));
 		printf("waiting...\n");
 		sleep(1);
 	}
 
-	printf("ftdi open succeeded: %d\n",f);
+	fprintf(stdout, "[+] ftdi open succeeded: %d\n",f);
 
 	f = ftdi_set_bitmode(ftdi, tab[led], BITMODE_CBUS);
 
 	if (f < 0){
-		fprintf(stderr, "set_bitmode failed for 0x%ld, error %d (%s)\n", tab[led], f, ftdi_get_error_string(ftdi));
+		fprintf(stderr, "[e] set_bitmode failed for 0x%ld, error %d (%s)\n", tab[led], f, ftdi_get_error_string(ftdi));
 		ftdi_usb_close(ftdi);
 		ftdi_free(ftdi);
 		exit(-1);
@@ -101,12 +101,13 @@ int set_led(int led){
 	f = ftdi_read_pins(ftdi, &buf[0]);
 
 	if (f < 0){
-		fprintf(stderr, "read_pins failed, error %d (%s)\n", f, ftdi_get_error_string(ftdi));
+		fprintf(stderr, "[e] read_pins failed, error %d (%s)\n", f, ftdi_get_error_string(ftdi));
 		ftdi_usb_close(ftdi);
 		ftdi_free(ftdi);
 		exit(-1);
 	}
 	printf("0x%01x\n", buf[0] & 0x0f);
+
 	//printf("disabling bitbang mode\n");
 	//ftdi_disable_bitbang(ftdi);
 
